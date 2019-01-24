9415ea1d31fa84cc81297fe4f5c22293cdbb5c68
Alex Benz <benzalexander@gmx.de>
2019-01-24 10:53:50 +0100

    added fetch data method

--- hlpr.js
+++ hlpr.js
@@ -1,69 +1,83 @@
 export const hlpr_getReadablePhone = phoneNr => {
-    const numbers = {
-      eins: 1,
-      zwei: 2,
-      drei: 3,
-      vier: 4,
-      fünf: 5,
-      sechs: 6,
-      sieben: 7,
-      acht: 8,
-      neun: 9,
-      null: 0
-    };
-  
-    let readablePhoneNr = phoneNr;
-  
-    for (let number in numbers) {
-      const regex = new RegExp(hlpr_escapeRegExp(number), 'g');
-      readablePhoneNr = readablePhoneNr.replace(regex, numbers[number]);
-    }
-  
-    return readablePhoneNr;
+  const numbers = {
+    eins: 1,
+    zwei: 2,
+    drei: 3,
+    vier: 4,
+    fünf: 5,
+    sechs: 6,
+    sieben: 7,
+    acht: 8,
+    neun: 9,
+    null: 0
   };
-  
-  export const hlpr_getReadableEMail = email => {
-    const mail = {
-      '[at]': '@',
-      '[punkt]': '.',
-      '[minus]': '-'
-    };
-    let readableEMail = email;
-  
-    for (let word in mail) {
-      const regex = new RegExp(hlpr_escapeRegExp(word), 'g');
-      readableEMail = readableEMail.replace(regex, mail[word]);
-    }
-    return readableEMail;
+
+  let readablePhoneNr = phoneNr;
+
+  for (let number in numbers) {
+    const regex = new RegExp(hlpr_escapeRegExp(number), 'g');
+    readablePhoneNr = readablePhoneNr.replace(regex, numbers[number]);
+  }
+
+  return readablePhoneNr;
+};
+
+export const hlpr_getReadableEMail = email => {
+  const mail = {
+    '[at]': '@',
+    '[punkt]': '.',
+    '[minus]': '-'
   };
-  
-  export const hlpr_escapeRegExp = string => {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  };
-  
-  export const hlpr_createId = (addOn = '') => {
-    return `${hlpr_createUUIDv4()}${addOn}`;
-  };
-  
-  export const hlpr_createUUIDv4 = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = (Math.random() * 16) | 0,
-        v = c === 'x' ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  };
-  
-  export const hlpr_formatValueToCurrency: formatValueToCurrency = (
-    value,
-    currency = 'EUR',
-    lang = 'de-DE'
-  ) => {
-    const biggerUnit = value / 100;
-    const formattedCurrency = biggerUnit.toLocaleString(lang, {
-      style: 'currency',
-      currency
-    });
-  
-    return formattedCurrency;
-  };
-  
+  let readableEMail = email;
+
+  for (let word in mail) {
+    const regex = new RegExp(hlpr_escapeRegExp(word), 'g');
+    readableEMail = readableEMail.replace(regex, mail[word]);
+  }
+  return readableEMail;
+};
+
+export const hlpr_escapeRegExp = string => {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
+
+export const hlpr_createId = (addOn = '') => {
+  return `${hlpr_createUUIDv4()}${addOn}`;
+};
+
+export const hlpr_createUUIDv4 = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
+export const hlpr_formatValueToCurrency = (
+  value,
+  currency = 'EUR',
+  lang = 'de-DE'
+) => {
+  const biggerUnit = value / 100;
+  const formattedCurrency = biggerUnit.toLocaleString(lang, {
+    style: 'currency',
+    currency
+  });
+
+  return formattedCurrency;
+};
+
+export const hlpr_fetchData = (url = ``, data = {}, method = 'POST') => {
+  return fetch(url, {
+    method,
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrer: 'no-referrer',
+    body: JSON.stringify(data)
+  }).then(response => response.json());
+};
